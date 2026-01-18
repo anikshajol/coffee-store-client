6eb189b79080686331ae67f53486947d97331b3d
anikshajol <anikshajol@gmail.com>
2026-01-18 14:47:12 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  // useEffect(() => {
+  //   document.title = "Home page";
+  // }, []);
+  return (
+    <>
+      <h2>hello</h2>
+      <button className="btn">Get start</button>
+    </>
+  );
+}
+
+export default App;
